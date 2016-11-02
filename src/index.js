59d1a1264051f6c84ab681b4a69f49a5adbc196a
Bjørn Arve Lagim <bjornarvelagim@MacBookWIFI.lan>
2016-11-02 17:35:54 +0100

    Heroku Fixes

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import { Provider } from 'react-redux';
 import { Router , browserHistory } from 'react-router';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-86750634-1');
 
 const store = configureStore(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+function fireTracking() {
+  ReactGA.pageview(window.location.hash);
+}
 render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes}/>
+    <Router onUpdate={fireTracking} history={browserHistory} routes={routes}/>
   </Provider>,
   document.getElementById('root')
 );
